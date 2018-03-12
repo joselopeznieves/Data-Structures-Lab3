aaf4f4c516fb714e0419c36464fb06872003dff3
4035c6 <4035c6@melon.ece.uprm.edu>
2018-03-12 14:58:25 -0400

    Implement SLFLList

--- LLLab4035_Students/src/linkedLists/SLFLList.java
+++ LLLab4035_Students/src/linkedLists/SLFLList.java
@@ -1,6 +1,7 @@
 package linkedLists;
 /**
  * Singly linked list with references to its first and its
+
  * last node. 
  * 
  * @author pirvos
@@ -10,6 +11,7 @@ package linkedLists;
 import java.util.NoSuchElementException;
 
 import linkedLists.LinkedList;
+import linkedLists.AbstractSLList.SNode;
 
 public class SLFLList<E> extends SLList<E>
 {
@@ -23,48 +25,82 @@ public class SLFLList<E> extends SLList<E>
 	
 	
 	public void addFirstNode(Node<E> nuevo) {
-		// TODO Auto-generated method stub
-		
+		((SNode<E>) nuevo).setNext(first);
+		first = (SNode<E>) nuevo;
+		if(length == 0) 
+			last = (SNode<E>) nuevo;
+		length++;
 	}
 
 	public void addNodeAfter(Node<E> target, Node<E> nuevo) {
-		// TODO Auto-generated method stub
-		
+		super.addNodeAfter(target, nuevo);
+		if(((SNode<E>) nuevo).getNext() == null)
+			last = (SNode<E>) nuevo;
+		length++;
 	}
 
 	public void addNodeBefore(Node<E> target, Node<E> nuevo) {
-		// TODO Auto-generated method stub
-		
+		if(target == first){
+			this.addFirstNode(nuevo);
+		}
+		else{
+			Node<E> prev = getNodeBefore(target);
+			addNodeAfter(prev, nuevo);
+		}
+		length++;
 	}
 
 	public Node<E> getFirstNode() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(first == null) throw new NoSuchElementException("There is no first node");
+		
+		return first;
 	}
 
 	public Node<E> getLastNode() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(last == null) throw new NoSuchElementException("There is no last node");
+		return last;
 	}
 
 	public Node<E> getNodeAfter(Node<E> target) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(((SNode<E>) target).getNext() == null) throw new NoSuchElementException("There is no node after");
+		return ((SNode<E>) target).getNext();
 	}
 
 	public Node<E> getNodeBefore(Node<E> target)
 			throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(target == first) throw new NoSuchElementException("There is no node before");
+		else{
+			SNode<E> prev = first; 
+			while (prev != null && prev.getNext() != target) 
+				prev = prev.getNext();  
+			return prev; 
+		}
 	}
 
 	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.length;
 	}
 
 	public void removeNode(Node<E> target) {
-		// TODO Auto-generated method stub
+		if (target == first) {
+			first = first.getNext();
+			if(first == null){
+				last = null;
+			}
+			else if (first.getNext() == null){
+				last = first;
+			}
+		}
+			 
+		else { 
+			SNode<E> prevNode = (SNode<E>) this.getNodeBefore(target); 
+			prevNode.setNext(((SNode<E>) target).getNext()); 
+			if(target == last){
+				last = prevNode;
+			}
+		}
+		((SNode<E>) target).clean();   // clear all references from target
+		length--; 
 		
 	}
 	
